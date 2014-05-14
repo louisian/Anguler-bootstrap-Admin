5fbdec555a7dc94409094d0d6a59930902468d7d
sithila123 <sithila@ceylonit.com>
2014-05-14 13:54:26 +0530

    chages with fontawsome and popup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 $( document ).ready(function() {
 $('#elem').popover();
+
+$("#elem").on('show.bs.popover',function () {
+    $('.popover.in').remove();
+});
+
+$('[data-toggle="popover"]').popover();
+
+$('body').on('click', function (e) {
+    //only buttons
+    if ($(e.target).data('toggle') !== 'popover'
+        && $(e.target).parents('.popover.in').length === 0) { 
+        $('[data-toggle="popover"]').popover('hide');
+    }
+    //buttons and icons within buttons
+    /*
+    if ($(e.target).data('toggle') !== 'popover'
+        && $(e.target).parents('[data-toggle="popover"]').length === 0
+        && $(e.target).parents('.popover.in').length === 0) { 
+        $('[data-toggle="popover"]').popover('hide');
+    }
+    */
+});
+
+
+
 });
